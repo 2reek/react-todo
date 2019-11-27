5c685b6b70bba73286d68ccc99843f69ab84ecc1
2reek <mountagest@gmail.com>
2019-11-27 12:39:42 +0300

    feat: study props(029)

--- src/components/todo-list.js
+++ src/components/todo-list.js
@@ -5,10 +5,10 @@ const TodoList = () => {
 	return (
 		<ul>
 			<li>
-				<TodoListItem />
+				<TodoListItem label="Drink Coffe" />
 			</li>
 			<li>
-				<TodoListItem />
+				<TodoListItem label="Build React App" important />
 			</li>
 		</ul>
 	);
